55a255c8629d9f614a740de87faaa15e880f368f
Nicolò Fiore <fiorenicolo05@gmail.com>
2023-03-16 09:14:19 +0100

    start project

---
+++ src/TestGit.java
@@ -0,0 +1,6 @@
+public class TestGit {
+    public static void main(String[] args) {
+        String[] test = {"Ciao", "prova","panino"};
+        TestGitSimple.print(test);
+    }
+}
---
+++ src/TestGitSimple.java
@@ -0,0 +1,8 @@
+public class TestGitSimple {
+
+    public static void print(String[] words){
+        for (String i: words) {
+            System.out.println(i);
+        }
+    }
+}
